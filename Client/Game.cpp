b5034ff4fe8f76d45a619ec020ebfd1b91b5d9ef
shinjeongmin <sin37minduke@naver.com>
2022-07-12 15:08:48 +0900

    Input and Timer

--- Client/Game.cpp
+++ Client/Game.cpp
@@ -47,13 +47,24 @@ void Game::Init(const WindowInfo& info)
 
 void Game::Update()
 {
+	GEngine->Update();
+
 	GEngine->RenderBegin();
 
 	shader->Update();
 
 	{
-		Transform t;
-		t.offset = Vec4(0.25f, 0.25f, 0.2f, 0.f);
+		static Transform t = {};
+
+		if (INPUT->GetButton(KEY_TYPE::W))
+			t.offset.y += 1.f * DELTA_TIME;
+		if (INPUT->GetButton(KEY_TYPE::S))
+			t.offset.y -= 1.f * DELTA_TIME;
+		if (INPUT->GetButton(KEY_TYPE::A))
+			t.offset.x -= 1.f * DELTA_TIME;
+		if (INPUT->GetButton(KEY_TYPE::D))
+			t.offset.x += 1.f * DELTA_TIME;
+
 		mesh->SetTransform(t);
 
 		mesh->SetTexture(texture);
@@ -61,7 +72,7 @@ void Game::Update()
 		mesh->Render();
 	}
 
-	{
+	/*{
 		Transform t;
 		t.offset = Vec4(0.f, 0.f, 0.3f, 0.f);
 		mesh->SetTransform(t);
@@ -69,7 +80,7 @@ void Game::Update()
 		mesh->SetTexture(texture);
 
 		mesh->Render();
-	}
+	}*/
 
 	GEngine->RenderEnd();
 }
